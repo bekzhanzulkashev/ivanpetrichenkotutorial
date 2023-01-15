b52b71c934172070d8bc75e3f2486fc978cf887f
Bekzhan <bekzhanzulkashev@gmail.com>
2023-01-15 22:04:55 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+// 'use strict';
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = +prompt('Are you 18 years old?', '12');
+// console.log(answer + 5);
+
+const answers = [];
+
+answers[0] = prompt('What is your name?', '');
+answers[1] = prompt('What is your last name?', '');
+answers[2] = prompt('How old are you?', '');
+
+document.write(answers);
